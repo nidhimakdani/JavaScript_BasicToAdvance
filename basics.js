76bc04e8157139876fffd5c342f01754102cf7fd
unknown <nidhimakdani@gmail.com>
2022-07-21 11:52:11 +0530

    Name changed

---
+++ basics.js
@@ -0,0 +1,126 @@
+// // @authore nidhimakdani
+// "use strict";  // Case sensitive
+
+// console.log("hello")
+
+// //Variables 
+// var user = 'nidhi'
+// console.log(user)
+
+// //Redecalriong value 
+// var user = 'kashish'
+// console.log(user)
+
+// let userSurname = 'makdani'
+// // let userSurname = 'makdani'
+// console.log(userSurname)
+
+// //Constants 
+// const pi = 3.14;
+// console.log(pi)
+
+// //string indexing
+// let userName = 'nidhi'
+// console.log(userName.length)
+// console.log(userName[0])
+// console.log(userName[userName.length - 1])
+
+// //string methods
+// var user = 'nidHi         '
+// console.log(user.length)
+// console.log(user.trim().length)
+// console.log(user.toUpperCase())
+// console.log(user.toLowerCase())
+// console.log(user.slice(0,3))
+
+// //****Operators
+// //*dataTypes(Primitives)
+// //string
+// //boolean
+// //undefine
+// //null
+// //BigInt
+// //number
+// //symbol
+
+// console.log(typeof 'nidhi')
+
+// //Number to string 
+// let age = 22;
+// console.log(age + '' ) // convert to string just add empty string with number 
+
+// let address = '78'
+// console.log(+address)
+
+// age = Number(age)
+// console.log(age)
+
+// //String concatination
+// // 'string1 ' +' ' +'string2'
+
+// //Backtics
+// console.log(`My name is ${userName} and my age is ${age}`)
+
+// //BigInt (Two methods to declare Big Int)
+// let num1 = BigInt(35);
+// let num2 = 5n; 
+// console.log(num1 + num2)
+
+// //Comparision Operators
+// // > , < , >= ,<=  booelean Output
+// // == and === 
+// //== will compare value 
+// // === will compare Datatype
+// // != vs ==!
+
+// //truthy falsy value
+// //falsy values 
+// //null
+// // ""
+// //0
+// //undefined
+// // example // a = 0 or undefined
+
+// //if else condition
+// age = 10
+// if(age < 12){
+//     console.log("eligible")
+// }
+// else{
+//     console.log("No")
+// }
+
+// //ternary operator
+// console.log(age<=12? 'milk':'coffee')
+
+// // and and or operator
+// if(user[0] === 'n' && age>9)
+// {
+//     console.log("Workes")
+// }
+// else{
+//     console.log("Wrong data")
+// }
+// // || or one condtion needs to satisfy
+
+// // IF ELse NEsTEd IF ELse and sWitch case 
+
+// // Loop concept
+// // while loop
+// let i = 0
+// while(i<=9)
+// {
+//     i ++ 
+// }
+// console.log(i)
+// // for loop
+// let num =10
+// for(let i = 1; i<=num ; i++){
+//     total = total + 1;
+// }
+// console.log(total)
+
+// // break and continue
+// // do while loop
+// // do this while (ye krna hai par kab tak karna hai)
+
